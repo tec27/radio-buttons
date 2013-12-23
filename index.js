58b1ddb7928402fb8554b66048cf826a0c38ec77
Travis Collins <travis@tec27.com>
2013-12-22 18:03:48 -0800

    Add handlers for player state messages.

--- index.js
+++ index.js
@@ -24,11 +24,27 @@ app.get('/favicon.ico', function(req, res) { res.send(404) })
 
 var server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
+
+io.set('log level', 2)
+
 server.listen(3000, function() {
   console.log('Server listening on :3000')
 })
 
-io.sockets.on('connection', function (socket) {
-  console.log('client connected.')
+io.of('/players').on('connection', function (socket) {
+  console.log('player connected.')
+  socket.on('buttons/play/state', function(enabled) {
+    console.log('play button enabled: ' + enabled)
+  }).on('buttons/rewind/state', function(enabled) {
+    console.log('rewind button enabled: ' + enabled)
+  }).on('buttons/forward/state', function(enabled) {
+    console.log('forward button enabled: ' + enabled)
+  }).on('playing', function(isPlaying) {
+    console.log('playing: ' + isPlaying)
+  })
   socket.emit('buttons/play')
+})
+
+io.of('/remotes').on('connection', function(socket) {
+  console.log('remote connected.')
 })
